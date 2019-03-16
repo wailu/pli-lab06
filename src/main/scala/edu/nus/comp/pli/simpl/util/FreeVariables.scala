12fa51a82f4fef9aee5b6c3acf888c242e325dca
wailunlim <wailun@u.nus.edu>
2019-03-17 00:39:09 +0800

    Fill up fv function

--- src/main/scala/edu/nus/comp/pli/simpl/util/FreeVariables.scala
+++ src/main/scala/edu/nus/comp/pli/simpl/util/FreeVariables.scala
@@ -17,16 +17,16 @@ object FreeVariables {
         fv(condition) union fv(consequent) union fv(alternative)
 
       case Func(_, formalArgs, body) =>
-        fv(body) // add you code here
+        fv(body) diff formalArgs.toSet
 
       case RecFunc(typ, name, formalArgs, body) =>
-        fv(body) // add you code here
+        fv(body) diff (formalArgs.toSet + name)
 
       case Appln(func, actualArgs) =>
-        fv(func)  // add you code here
+        fv(func) union actualArgs.toSet.flatMap(e => fv(e))
 
       case Let(defs, _, body) =>
-        fv(body)  // add you code here
+        fv(body) diff defs.map(x => x._2).toSet union (defs.flatMap(x => fv(x._3)).toSet diff defs.map(x => x._2).toSet)
     }
   }
 }
