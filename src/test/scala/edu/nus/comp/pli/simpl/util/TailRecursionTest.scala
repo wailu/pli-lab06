48ceaf3f40d89ad80de90afde0cfce95c2299e4a
wailunlim <wailun@u.nus.edu>
2019-04-13 10:38:51 +0800

    Update expected VM instructions for TailRecursionTest

--- src/test/scala/edu/nus/comp/pli/simpl/util/TailRecursionTest.scala
+++ src/test/scala/edu/nus/comp/pli/simpl/util/TailRecursionTest.scala
@@ -52,7 +52,7 @@ class TailRecursionTest extends FlatSpec {
         case None =>
           print ("type inference error")
           false
-        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(1), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("x",0)), LD(("z",1)), TIMES, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), CALL(2), LABEL("labal_4"), RTN)
+        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(1), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("x",0)), LD(("z",1)), TIMES, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), TAILCALL(2), LABEL("labal_4"), RTN)
 
       }
     )
@@ -65,7 +65,7 @@ class TailRecursionTest extends FlatSpec {
         case None =>
           print ("type inference error")
           false
-        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(0), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("x",0)), LD(("z",1)), PLUS, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), CALL(2), LABEL("labal_4"), RTN)
+        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(0), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("x",0)), LD(("z",1)), PLUS, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), TAILCALL(2), LABEL("labal_4"), RTN)
 
       }
     )
@@ -77,7 +77,7 @@ class TailRecursionTest extends FlatSpec {
         case None =>
           print ("type inference error")
           false
-        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(128), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("z",1)), LD(("x",0)), DIV, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), CALL(2), LABEL("labal_4"), RTN)
+        case Some (t) => compile(parse(source)) ==List(LDFR(List(),("recurse",0),4,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(128), LDF(Vector(),2,"labal_1"), LDCI(3), LDCI(2), LD(("recurse",0)), TAILCALL(4), LABEL("labal_1"), LD(("z",1)), LD(("x",0)), DIV, RTN, LABEL("labal_2"), LD(("y",2)), LDCI(0), EQ, JOF("labal_3"), LD(("initvalue",4)), GOTO("labal_4"), LABEL("labal_3"), LD(("initvalue",4)), LD(("operation",3)), LD(("y",2)), LDCI(1), MINUS, LD(("x",1)), LD(("recurse",0)), CALL(4), LD(("x",1)), LD(("operation",3)), TAILCALL(2), LABEL("labal_4"), RTN)
 
       }
     )
@@ -108,6 +108,161 @@ class TailRecursionTest extends FlatSpec {
       }
     )
   }
+  "test16.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test16.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDCI(3), LDFR(Vector(),("f",0),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LD(("n",1)), LD(("f",0)), TAILCALL(1))
 
+      }
+    )
+  }
+  "test17.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test17.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDCI(3), LDF(Vector(),1,"labal_1"), LDF(Vector(),2,"labal_0"), CALL(2), DONE, LABEL("labal_0"), LD(("y",1)), LD(("x",0)), TAILCALL(1), LABEL("labal_1"), LD(("x",0)), LDCI(1), PLUS, RTN)
+
+      }
+    )
+  }
+  "test19.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test19.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDFR(Vector(),("facloop",0),2,"labal_3"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDF(Vector(("facloop",0)),1,"labal_2"), LDF(Vector(),1,"labal_1"), TAILCALL(1), LABEL("labal_1"), LDCI(4), LD(("fac",0)), TAILCALL(1), LABEL("labal_2"), LDCI(1), LD(("n",1)), LD(("facloop",0)), TAILCALL(2), LABEL("labal_3"), LD(("n",1)), LDCI(1), EQ, JOF("labal_4"), LD(("acc",2)), GOTO("labal_5"), LABEL("labal_4"), LD(("acc",2)), LD(("n",1)), TIMES, LD(("n",1)), LDCI(1), MINUS, LD(("facloop",0)), TAILCALL(2), LABEL("labal_5"), RTN)
+
+      }
+    )
+  }
+  "test20.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test20.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDF(Vector(),1,"labal_1"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(3), LD(("fac",0)), TAILCALL(1), LABEL("labal_1"), LD(("n",0)), LDCI(1), PLUS, RTN)
+
+      }
+    )
+  }
+  "test21.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test21.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDFR(Vector(),("foo",0),1,"labal_1"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LD(("n",1)), LDCI(0), EQ, JOF("labal_2"), LDCI(0), GOTO("labal_3"), LABEL("labal_2"), LDCI(2), LD(("n",1)), LDCI(1), MINUS, LD(("foo",0)), CALL(1), PLUS, LABEL("labal_3"), RTN)
+
+      }
+    )
+  }
+  "test22.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test22.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDFR(Vector(),("foo",0),1,"labal_1"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LD(("n",1)), LDCI(0), EQ, JOF("labal_2"), LDCI(0), GOTO("labal_3"), LABEL("labal_2"), LD(("n",1)), LDCI(1), MINUS, LD(("foo",0)), CALL(1), LD(("n",1)), PLUS, LABEL("labal_3"), RTN)
+
+      }
+    )
+  }
+  "test23.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test23.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDFR(Vector(),("foo",0),1,"labal_1"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(1), LDCI(30), LD(("f",0)), TAILCALL(2), LABEL("labal_1"), LD(("n",1)), LDCI(0), EQ, JOF("labal_2"), LDF(Vector(),1,"labal_4"), GOTO("labal_3"), LABEL("labal_2"), LDF(Vector(("foo",0), ("n",1)),1,"labal_5"), LABEL("labal_3"), RTN, LABEL("labal_4"), LD(("acc",0)), RTN, LABEL("labal_5"), LD(("acc",2)), LD(("n",1)), TIMES, LD(("n",1)), LDCI(1), MINUS, LD(("foo",0)), TAILCALL(2))
+
+      }
+    )
+  }
+  "test24.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test24.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDCI(3), LDF(Vector(),1,"labal_1"), CALL(1), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LDF(Vector(("x",0)),1,"labal_2"), RTN, LABEL("labal_2"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
+  "test25.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test25.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDCI(3), LDF(Vector(),1,"labal_1"), CALL(1), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LDF(Vector(("x",0)),1,"labal_2"), RTN, LABEL("labal_2"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
+  "test26.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test26.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDCI(3), LDF(Vector(),1,"labal_1"), CALL(1), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LDF(Vector(("x",0)),1,"labal_2"), RTN, LABEL("labal_2"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
+  "test27.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test27.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) ==List(LDF(Vector(),1,"labal_1"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(4), LD(("f",0)), TAILCALL(1), LABEL("labal_1"), LDF(Vector(("x",0)),1,"labal_2"), RTN, LABEL("labal_2"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
+  "test28.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test28.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) == List(LDF(Vector(),1,"labal_2"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDF(Vector(("f",0)),1,"labal_1"), RTN, LABEL("labal_1"), LD(("pa",1)), LDCI(4), LD(("f",0)), TAILCALL(2), LABEL("labal_2"), LDF(Vector(("x",0)),1,"labal_3"), RTN, LABEL("labal_3"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
+  "test29.simpl" should "has no free variable" in {
+    val source = Source.fromURL(getClass.getResource("/simpl/test29.simpl")).mkString
+    assert(
+      type_infer(Seq(),parse(source)) match{
+        case None =>
+          print ("type inference error")
+          false
+        case Some (t) => compile(parse(source)) == List(LDF(Vector(),1,"labal_3"), LDF(Vector(),1,"labal_0"), CALL(1), DONE, LABEL("labal_0"), LDCI(3), LDF(Vector(("f",0)),1,"labal_2"), CALL(1), LDF(Vector(),1,"labal_1"), TAILCALL(1), LABEL("labal_1"), LDCI(4), LD(("g",0)), TAILCALL(1), LABEL("labal_2"), LDCI(3), LD(("f",0)), TAILCALL(1), LABEL("labal_3"), LDF(Vector(("x",0)),1,"labal_4"), RTN, LABEL("labal_4"), LD(("x",0)), LD(("y",1)), PLUS, RTN)
+
+      }
+    )
+  }
 
 }
